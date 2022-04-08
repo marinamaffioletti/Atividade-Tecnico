a510dff733f8b683eced775ba787ae79f811efc8
Marina <103116403+marinamaffioletti@users.noreply.github.com>
2022-04-08 15:17:37 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GitHub</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+ 
+
+    <body>
+
+   <section id="titulo"><h2>Oii, tudo bem?</h1></section>
+   <section id="texto"><h4> Essa é uma atividade do técnico. Não sei mais oq escrever. Bj </h4></section>
+   
+   <section id="imagem"><img src="gatinho.png" alt=""></section>
+
+    </body>
+<html>
